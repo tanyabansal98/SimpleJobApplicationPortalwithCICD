d34a80b1d402da3bfe49c8a4d5f7fa247ad9fe65
Tanya Bansal <254046972+tanyabansal98@users.noreply.github.com>
2026-06-27 20:11:10 -0400

    Fix SessionFactory import in HibernateConfig

--- backend/src/main/java/com/job/portal/config/HibernateConfig.java
+++ backend/src/main/java/com/job/portal/config/HibernateConfig.java
@@ -1,5 +1,6 @@
 package com.job.portal.config;
 
+import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
